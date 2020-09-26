4dae58d3cd4a6b56c6b7326d1924c33a21726cdb
Nipurna Dhakal <nipurna@Nipurnas-MacBook-Pro.local>
2020-09-26 15:35:20 -0400

    Web App update

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require("express");
-const mysql = require("mysql")
+const path = require('path')
+const mysql = require("mysql");
+const dotenv = require('dotenv');
+dotenv.config({path:'./.env'});
 
 const app = express();
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'nodejs-login'
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    // database: 'shoe-login',
+    database: process.env.DATABASE,
+    socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
 });
+
+const publicDirectory = path.join(__dirname,'./public');
+app.use(express.static(publicDirectory));
+app.set('view engine', 'hbs')
 
 db.connect((error)=>{
     if(error){
@@ -19,10 +28,11 @@ db.connect((error)=>{
 })
 
 app.get("/", (req,res)=>{
-    res.send("<h1>Home Page</h1>")
+    //res.send("<h1>Home Page</h1>")
+    res.render("index")
 });
 
-app.listen(5000, () =>{
-    console.log("Server statred on Port 5000");
+app.listen(5001, () =>{
+    console.log("Server statred on Port 5001");
 }
 )
