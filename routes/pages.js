59ebc44e86eb01da69b7ddbef0f2a19bf4e0b86c
Nipurna Dhakal <nipurna@Nipurnas-MacBook-Pro.local>
2020-09-26 16:20:00 -0400

    Web App update

--- routes/pages.js
+++ routes/pages.js
@@ -1,16 +1,16 @@
-// const { Router } = require('express');
-// const express = require('express');
+const { Router } = require('express');
+const express = require('express');
 
-// const rounter = express.Router();
+const rounter = express.Router();
 
-// Router.length('/', (req,res)=>{
-//     res.render('render');
+Router.length('/', (req,res)=>{
+    res.render('render');
 
-// });
-// Router.length('/'register, (req,res)=>{
-//     res.render('register');
+});
+Router.length('/'register, (req,res)=>{
+    res.render('register');
     
-// });
+});
 
-// module.expoets = router;
+module.expoets = router;
 
