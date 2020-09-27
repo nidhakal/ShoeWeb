b770bfa41d757ac508826fa976d355367422720f
Joshua Ofosu-Amoani <j.ofosuamoani@yahoo.com>
2020-09-26 20:58:55 -0400

    Store inforomation in database

--- controllers/auth.js
+++ controllers/auth.js
@@ -34,7 +34,16 @@ exports.register = (req, res) => {
         let hashedPassword = await bcrypt.hash(password, 8);
         console.log(hashedPassword);
 
-        
+        db.query("INSERT INTO users SET ?", {name: name, email: email, password: hashedPassword}, (error, results) => {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(results);
+                return res.render("register", {
+                    message: "User registered"
+                })
+            }
+        })
 
 
     });
