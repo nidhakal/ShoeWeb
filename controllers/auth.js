6cbaf8c03e1d5d478c5da200c827379519ea7140
Joshua Ofosu-Amoani <j.ofosuamoani@yahoo.com>
2020-09-26 20:09:50 -0400

    Hashing Passwords

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,41 @@
+const mysql = require("mysql");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    // database: 'shoe-login',
+    database: process.env.DATABASE,
+    socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+});
+
 exports.register = (req, res) => {
     console.log(req.body);
-    res.send("Form submitted");
+
+    const { name, email, password, passwordConfirm } = req.body;
+
+    db.query("SELECT email FROM users WHERE email = ?", [email], async (error, results) => {
+        if (error) {
+            console.log(error);
+        }
+
+        if (results.length > 0) {
+            return res.render("register", {
+                message: "That email is already in use"
+            });
+        } else if (password !== passwordConfirm) {
+            return res.render("register", {
+                message: "Passwords do not match"
+            });
+        }
+        
+        let hashedPassword = await bcrypt.hash(password, 8);
+        console.log(hashedPassword);
+
+        
+
+
+    });
 }
